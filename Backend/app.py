bb3fc3620d538aacec5eae37aab96507a724a676
Hritajit Sur <122168466+Hrita0910@users.noreply.github.com>
2025-08-19 22:41:49 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # Enable CORS for React + Vite frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173" , "https://pdf-analyze-rose.vercel.app" , "http://localhost:8080"],  # or "*" in dev
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
